ef931fb3939d091a2f81d11eaf37b847610468e1
Dusan Orlovic <duleorlovic@gmail.com>
2019-02-23 09:47:18 +0100

    Add sample page and fontello

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root page
+  root 'pages#home'
 end
