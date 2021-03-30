06fb529bb5892b94b70cea5eda7fa9bf56933b37
Dusan Orlovic <duleorlovic@gmail.com>
2021-03-30 14:23:29 +0200

    Add config/etc files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock '~> 3.11.2'
 
-set :application, 'move_index'
+set :application, 'movebase'
 set :repo_url, 'git@github.com:trkin/move-index.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/#{fetch(:application)}"
+set :deploy_to, "/home/ubuntu/#{fetch(:application)}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -38,3 +38,6 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+set :rbenv_ruby, '2.6.3'
+set :rbenv_type, :user
